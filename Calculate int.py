7575d14eacba6a892f04baad19af76976a2323b3
jahnavipurimitla6-glitch <jahnavipurimitla6@gmail.com>
2025-12-30 16:18:12 +0530

    Create Calculate int.py

---
+++ Calculate int.py
@@ -0,0 +1,10 @@
+def Calculate(m,n):
+    total = 0
+    for i in range(m,n+1):
+        if i % 15 == 0:
+            total +=i
+    return total
+m = int(input("Enter m:"))
+n = int(input("Enter n:"))
+result = Calculate(m,n)
+print("sum =",result)
